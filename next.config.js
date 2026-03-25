6a6bc2c637ebdc94b5835d9a0681984ec43b7e3b
elnaizak <elnaizak.76@gmail.com>
2026-03-25 13:08:12 +0200

    final for demo 25 v3

--- next.config.js
+++ next.config.js
@@ -3,10 +3,19 @@
 
 // export default nextConfig;
 
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // Add these blocks:
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has TypeScript errors.
+      ignoreBuildErrors: true,
+    },
   }
   
- module.exports = nextConfig;
+module.exports = nextConfig;
